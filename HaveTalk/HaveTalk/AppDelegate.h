96e1fc0f3cd19df04f5de6109c089667cb5e281e
rongxindeng <516763478@qq.com>
2016-08-01 17:40:09 +0800

    init project

---
+++ HaveTalk/HaveTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HaveTalk
+//
+//  Created by Chair on 16/8/1.
+//  Copyright © 2016年 tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
